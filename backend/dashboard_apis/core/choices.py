571b7baa6a5a15815b8472ad7b2c1152ff7d1fbe
Siddhartha <siddharthakumarsingh946@gmail.com>
2023-01-08 05:54:14 +0530

    initial models setup

---
+++ backend/dashboard_apis/core/choices.py
@@ -0,0 +1,10 @@
+RIDER_STATUS =[
+    ('trip','Trip'),
+    ('return','Return'),
+    ('at warehouse','At Warehouse')
+]
+ORDER_STATUS = [
+    ('delievered','Delievered'),
+    ('undelievered','Undelievered'),
+    ('delayed','Delayed')
+]
