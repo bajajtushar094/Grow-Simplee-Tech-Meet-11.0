4ce744f389403e04bc9aa799783056f762aa9c88
Siddhartha <siddharthakumarsingh946@gmail.com>
2023-01-13 20:29:07 +0530

    api 2 created

---
+++ backend/dashboard_apis/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from . import order_view
+
+
+urlpatterns = [
+    #test api
+    path('',views.getData),
+
+    #product api
+    path('product/<str:id>',order_view.order_detail_view)
+]
