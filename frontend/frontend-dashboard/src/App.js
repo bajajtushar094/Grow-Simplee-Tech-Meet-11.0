90a2f2a4d3e6badde846afa58fa9f18da8ff63be
dittapandey <adityapandey51020@gmail.com>
2023-01-21 23:49:45 +0530

    Dashboard page map portion remaining

--- frontend/frontend-dashboard/src/App.js
+++ frontend/frontend-dashboard/src/App.js
@@ -4,6 +4,7 @@ import Inventory from "./Pages/Inventory";
 import InventoryWebcam from "./Pages/InventoryWebcam";
 
 import Layout, { InventoryLayout } from "./Component/Layout";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Inventory />} />
         <Route path="/vol" element={<InventoryWebcam />} />
+        <Route path="/about" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
