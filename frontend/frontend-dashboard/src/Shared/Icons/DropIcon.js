d21a8014a9d43bdab7adc454d36e38c3ed51afdb
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2023-01-22 15:05:10 +0530

    inventory table completed

---
+++ frontend/frontend-dashboard/src/Shared/Icons/DropIcon.js
@@ -0,0 +1,11 @@
+import Icon from "./IconBase";
+
+const DropIcon = (props) => (
+  <Icon width="36" height="37" viewBox="0 0 36 37" fill="none"  {...props}>
+    <rect y="0.914062" width="36" height="36" rx="18" fill="#C2E7FF"/>
+<path d="M18 9.52039L26.1352 14.2172V23.6109L18 28.3077L9.86487 23.6109V14.2172L18 9.52039Z" fill="#3544B6"/>
+<path fillRule="evenodd" clipRule="evenodd" d="M26.1134 14.2204L18.0001 18.8172L9.87292 14.2126L17.9892 9.52039L26.1134 14.2172V14.2204Z" fill="#6D79D2"/>
+  </Icon>
+);
+
+export default DropIcon;
