3c53f1ba50cdf1bc4fc7b2c3564f9fd26632e230
tushar <tbajaj@iitg.ac.in>
2023-02-03 01:16:38 +0530

    added urls for auth

--- backend/dashboard_apis/core/urls.py
+++ backend/dashboard_apis/core/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
 from . import order_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     # test api
     path('', views.getData.as_view()),
+
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # product api
     path('product/<str:id>', order_view.order_detail.as_view()),
