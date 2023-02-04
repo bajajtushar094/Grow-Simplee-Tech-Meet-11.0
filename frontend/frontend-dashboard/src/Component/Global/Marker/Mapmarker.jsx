8a7be30a12a4f418ee3b1f5500ce743e94443adb
raj <rajinertia9239111@gmail.com>
2023-02-04 16:58:37 +0530

    added percentage label

--- frontend/frontend-dashboard/src/Component/Global/Marker/Mapmarker.jsx
+++ frontend/frontend-dashboard/src/Component/Global/Marker/Mapmarker.jsx
@@ -4,7 +4,7 @@ import Pins from '../../Mobile/Pins.svg'
 import rider from './rider.svg'
 function Mapmarker(props) {
   return (
-    <div style={{display:"flex", flexDirection:"row"}}>
+    <div style={{display:"flex", flexDirection:"row",height:'50px',width:"80px"}}>
       
        <div style={{position:'absolute'}}>
            <img src={Pins} alt="" />
@@ -12,6 +12,9 @@ function Mapmarker(props) {
 
        <div style={{marginLeft:"15px",marginTop:'10px', position:'absolute', height:'24px', width:'24px'}}>
             <img   src={props.image} alt="" />
+       </div>
+       <div style={{backgroundColor:'#3CB371',height:"18px",width:'35px',marginLeft:"35px",zIndex:'99',borderRadius:"8px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+            <p style={{color:'white', fontSize:'10px'}}>17%</p>
        </div>
     </div>
   
