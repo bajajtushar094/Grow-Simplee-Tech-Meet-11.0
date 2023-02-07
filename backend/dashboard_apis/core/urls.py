de6b9a2f2bf93d740d9af5b4c5cee3564286613f
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-02-07 18:35:02 +0530

    added url

--- backend/dashboard_apis/core/urls.py
+++ backend/dashboard_apis/core/urls.py
@@ -8,8 +8,11 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    # jwt apis
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='taken_refresh'),
+
     # test api
-    path('', views.getData.as_view()),
 
     # auth api
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -18,21 +21,32 @@ urlpatterns = [
     # product api
     path('product/<str:id>', order_view.order_detail.as_view()),
     path('product/<str:id>', order_view.order_image_update.as_view()),
-    path('upload/', views.upload.as_view()),
+    # path('upload/', views.upload.as_view()),
+    # path('images/', views.imageView.as_view()),
+    # path('uploadimages/', views.uploadImages.as_view()),
+
     path('orders/', order_view.get_all_orders.as_view()),
-    path('orders/json/<str:folder>', views.getFolder.as_view()),
 
     # rider api
-    path('rider-rewards/', views.rider_rewards.as_view()),
+    # path('rider-rewards/', views.rider_rewards.as_view()),
     path('rider-management/', views.getRiderManagementMap.as_view()),
-    path('orders/all', views.getOrder.as_view()),
-    path('riders/all', views.getRider.as_view()),
+    path('orders/all', views.getOrders.as_view()),
+    path('orders/json/<str:folder>', views.getFolder.as_view()),
+    path('riders/all', views.getRiders.as_view()),
     path('orders/rider/<str:id>', views.getRiderOrders.as_view()),
     path('rider/<str:id>', views.getRiderById.as_view()),
     path('bags/all', views.getBags.as_view()),
+    path('manager', views.getManager.as_view()),
     path('orders/cancel', views.cancelOrder.as_view()),
+    path('orders/upcoming', views.getUpcomingCount.as_view()),
     path('orders/add', views.addDynamicPickup.as_view()),
+    path('count/riders', views.countRiders.as_view()),
+    # To solve the first instance of VRP
+    path('solve/', views.generateInitialSolution.as_view()),
+    path('bin-packing/<str:id>', views.binPacking.as_view()),
 
+
+    path('orders/add', views.addDynamicPickup.as_view()),
     path('solve_initial/', views.generateInitialSolution.as_view()),
     path('solve/',views.generateRerouteSolution.as_view()),
     path('check_task_status/<str:task_id>',views.checkCeleryStatus.as_view()),
